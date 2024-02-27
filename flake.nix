ecd5bcc557e4348b30412fa4b1b0d4482bd41068
Michael Bryant <shadow53@shadow53.com>
2024-02-27 00:41:05 -0800

    chore: move package overrides to overlay

--- flake.nix
+++ flake.nix
@@ -15,17 +15,9 @@
         default = mkPoetryApplication { projectDir = self; };
       });
 
-      overlays = {
-        default = final: prev: {
-          calibre-web = self.packages.${prev.system}.default;
-        };
-      };
-
-      devShells = forAllSystems (system: let
-        inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryEnv defaultPoetryOverrides;
-
+      overlays = let
         addModules = (self: super: modules: builtins.listToAttrs (map (obj: let
-          extras = [self.flit-core] ++ (map (name: self.${name}) obj.modules) ++ (map (name: pkgs.${system}.${name}) (obj.pkgs or []));
+          extras = [self.flit-core] ++ (map (name: self.${name}) obj.modules) ++ (map (name: super.${name}) (obj.pkgs or []));
         in {
           name = obj.name;
           value = super.${obj.name}.overridePythonAttrs (old: {
@@ -43,58 +35,131 @@
           }) names)
         ));
       in {
+        default = final: prev: {
+          calibre-web = self.packages.${prev.system}.default;
+          comicapi = prev.comicapi.overridePythonAttrs (old: {
+            format = "setuptools";
+            src = final.fetchFromGitHub {
+              owner = "comictagger";
+              repo = old.pname;
+              rev = "2bf8332114e49add0bbc0fd3d85bdbba02de3d1a";
+              hash = "sha256-Cd3ILy/4PqWUj1Uu9of9gCpdVp2R6CXjPOuSXgrB894=";
+            };
+          });
+          cryptography = prev.cryptography.overridePythonAttrs (old: rec {
+            cargoDeps = pkgs.rustPlatform.fetchCargoTarball {
+              inherit (old) src;
+              name = "${old.pname}-${old.version}";
+              sourceRoot = "${old.pname}-${old.version}/${cargoRoot}";
+              sha256 = "sha256-Pw3ftpcDMfZr/w6US5fnnyPVsFSB9+BuIKazDocYjTU=";
+            };
+            cargoRoot = "src/rust";
+          });
+          levenshtein = prev.levenshtein.overridePythonAttrs (old: {
+            dontUseCmakeConfigure = true;
+            nativeBuildInputs = old.nativeBuildInputs ++ [
+              final.cmake
+              final.scikit-build
+            ];
+          });
+        } // (addModules final prev [
+          { name = "flask_dance"; modules = ["flit-core" "flit_core"]; }
+          { name = "flask-dance"; modules = ["flit-core" "flit_core"]; }
+          { name = "flask-oidc"; modules = ["poetry"]; }
+          { name = "wtforms"; modules = ["babel" "hatchling"]; }
+        ]) // (addSetuptools final prev [
+          "inflate64"
+          "multivolumefile"
+          "pybcj"
+          "pyppmd"
+          "pyzstd"
+          "py7zr"
+          "urlobject"
+          "free-proxy"
+          "rauth"
+          "flask-dance"
+          "flask-simpleldap"
+          "goodreads"
+          "text2digits"
+          "wordninja"
+          "scholarly"
+        ]);
+      };
+
+      devShells = forAllSystems (system: let
+        inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryEnv defaultPoetryOverrides;
+
+        #addModules = (self: super: modules: builtins.listToAttrs (map (obj: let
+        #  extras = [self.flit-core] ++ (map (name: self.${name}) obj.modules) ++ (map (name: pkgs.${system}.${name}) (obj.pkgs or []));
+        #in {
+        #  name = obj.name;
+        #  value = super.${obj.name}.overridePythonAttrs (old: {
+        #    buildInputs = (old.buildInputs or []) ++ extras;
+        #    nativeBuildInputs = (old.nativeBuildInputs or []) ++ extras;
+        #  });
+        #}) modules ));
+
+        #addSetuptools = (self: super: names: (addModules
+        #  self
+        #  super
+        #  (map (name: {
+        #    inherit name;
+        #    modules = [ "setuptools" ];
+        #  }) names)
+        #));
+      in {
         default = pkgs.${system}.mkShellNoCC {
           packages = with pkgs.${system}; [
             (mkPoetryEnv {
-              overrides = defaultPoetryOverrides.extend (self: super: {
-                  comicapi = super.comicapi.overridePythonAttrs (old: {
-                    format = "setuptools";
-                    src = fetchFromGitHub {
-                      owner = "comictagger";
-                      repo = old.pname;
-                      rev = "2bf8332114e49add0bbc0fd3d85bdbba02de3d1a";
-                      hash = "sha256-Cd3ILy/4PqWUj1Uu9of9gCpdVp2R6CXjPOuSXgrB894=";
-                    };
-                  });
-                  cryptography = super.cryptography.overridePythonAttrs (old: rec {
-                    cargoDeps = pkgs.${system}.rustPlatform.fetchCargoTarball {
-                      inherit (old) src;
-                      name = "${old.pname}-${old.version}";
-                      sourceRoot = "${old.pname}-${old.version}/${cargoRoot}";
-                      sha256 = "sha256-Pw3ftpcDMfZr/w6US5fnnyPVsFSB9+BuIKazDocYjTU=";
-                    };
-                    cargoRoot = "src/rust";
-                  });
-                  levenshtein = super.levenshtein.overridePythonAttrs (old: {
-                    dontUseCmakeConfigure = true;
-                    nativeBuildInputs = old.nativeBuildInputs ++ [
-                      self.cmake
-                      self.scikit-build
-                    ];
-                  });
-                } // (addModules self super [
-                  { name = "flask_dance"; modules = ["flit-core" "flit_core"]; }
-                  { name = "flask-dance"; modules = ["flit-core" "flit_core"]; }
-                  { name = "flask-oidc"; modules = ["poetry"]; }
-                  { name = "wtforms"; modules = ["babel" "hatchling"]; }
-                ]) // (addSetuptools self super [
-                  "inflate64"
-                  "multivolumefile"
-                  "pybcj"
-                  "pyppmd"
-                  "pyzstd"
-                  "py7zr"
-                  "urlobject"
-                  "free-proxy"
-                  "rauth"
-                  "flask-dance"
-                  "flask-simpleldap"
-                  "goodreads"
-                  "text2digits"
-                  "wordninja"
-                  "scholarly"
-                ])
-              );
+              #overrides = defaultPoetryOverrides.extend (self: super: {
+              #    comicapi = super.comicapi.overridePythonAttrs (old: {
+              #      format = "setuptools";
+              #      src = fetchFromGitHub {
+              #        owner = "comictagger";
+              #        repo = old.pname;
+              #        rev = "2bf8332114e49add0bbc0fd3d85bdbba02de3d1a";
+              #        hash = "sha256-Cd3ILy/4PqWUj1Uu9of9gCpdVp2R6CXjPOuSXgrB894=";
+              #      };
+              #    });
+              #    cryptography = super.cryptography.overridePythonAttrs (old: rec {
+              #      cargoDeps = pkgs.${system}.rustPlatform.fetchCargoTarball {
+              #        inherit (old) src;
+              #        name = "${old.pname}-${old.version}";
+              #        sourceRoot = "${old.pname}-${old.version}/${cargoRoot}";
+              #        sha256 = "sha256-Pw3ftpcDMfZr/w6US5fnnyPVsFSB9+BuIKazDocYjTU=";
+              #      };
+              #      cargoRoot = "src/rust";
+              #    });
+              #    levenshtein = super.levenshtein.overridePythonAttrs (old: {
+              #      dontUseCmakeConfigure = true;
+              #      nativeBuildInputs = old.nativeBuildInputs ++ [
+              #        self.cmake
+              #        self.scikit-build
+              #      ];
+              #    });
+              #  } // (addModules self super [
+              #    { name = "flask_dance"; modules = ["flit-core" "flit_core"]; }
+              #    { name = "flask-dance"; modules = ["flit-core" "flit_core"]; }
+              #    { name = "flask-oidc"; modules = ["poetry"]; }
+              #    { name = "wtforms"; modules = ["babel" "hatchling"]; }
+              #  ]) // (addSetuptools self super [
+              #    "inflate64"
+              #    "multivolumefile"
+              #    "pybcj"
+              #    "pyppmd"
+              #    "pyzstd"
+              #    "py7zr"
+              #    "urlobject"
+              #    "free-proxy"
+              #    "rauth"
+              #    "flask-dance"
+              #    "flask-simpleldap"
+              #    "goodreads"
+              #    "text2digits"
+              #    "wordninja"
+              #    "scholarly"
+              #  ])
+              #);
               projectDir = self;
             })
             poetry
